7b8818058c17747178da5283a1931aab01385e32
ahad <abdulahad95460@gmail.com>
2026-09-09 22:08:25 +0530

    configure app middleware

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
+   const authMiddleware = require("./middleware/authMiddleware");
 
 const app = express();
-app.use(express.json());
-connectDB()
+     app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+      app.get("/api/test", authMiddleware, (req, res) => {
+    res.json({
+        message: "You are authenticated",
+        userId: req.userId
+    });
+});
 app.get("/", (req, res) => {
     res.send("ProjectFlow API is running");
 });
 
+connectDB()
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`ProjectFlow server is running on port ${PORT}`);
