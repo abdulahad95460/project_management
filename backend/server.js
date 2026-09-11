810b53447064e6aae8b9c540a3ff9487144191ae
ahad <abdulahad95460@gmail.com>
2026-09-11 11:47:34 +0530

    update baseURL to Render, open CORS for production

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const authRoutes = require("./routes/authRoutes");
 const memberRoutes = require("./routes/memberRoutes");
 
   const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: false }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
